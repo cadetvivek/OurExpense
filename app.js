f958d797a74e75d3dca1987c03883ea97dda2959
cadetvivek <kushwahvivek805@gmail.com>
2024-10-05 16:00:39 +0530

    Expense Tracker

--- app.js
+++ app.js
@@ -11,12 +11,14 @@ const sequelize = require('./util/database');
 dotenv.config();
 const PORT = process.env.PORT;
 
+//model inports
 const Expenses = require('./models/expenses');
 const User = require('./models/users');
 const Orders = require('./models/orders');
 const Forgotpasswords = require('./models/forgotpasswords');
 const Downloads = require('./models/downloads');
 
+//Router imports
 const mainPageRouter = require('./routes/mainpage');
 const userRouter = require('./routes/user');
 const expenseRouter = require('./routes/expenses');
@@ -24,9 +26,13 @@ const purchaseRouter = require('./routes/purchase');
 const premiumRouter = require('./routes/premium');
 const passwordRouter = require('./routes/password');
 
+//Logging Setup:
 const accessLogStream = fs.createWriteStream('./access.log', { flags: 'a' });
 
+//App Configuration:
 const app = express();
+
+//Middleware Setup:
 app.use(morgan('combined', { stream: accessLogStream }));
 app.use(cors());
 app.use(express.json());
@@ -34,6 +40,7 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.static('public'));
 
 
+//Database Model Relationships:
 User.hasMany(Expenses);
 Expenses.belongsTo(User,{constraints:true, onDelete:'CASCADE'});
 
@@ -46,6 +53,7 @@ Forgotpasswords.belongsTo(User,{constraints:true,onDelete:'CASCADE'});
 User.hasMany(Downloads);
 Downloads.belongsTo(User,{constraints:true,onDelete:'CASCADE'});
 
+//Route Setup:
 app.use(mainPageRouter)
 app.use('/user',userRouter);
 app.use('/purchase',purchaseRouter);
@@ -53,6 +61,8 @@ app.use('/expenses',expenseRouter);
 app.use('/premium',premiumRouter);
 app.use('/password',passwordRouter);
 
+
+//Server Initialization:
 async function initiate(){
     try {
         await sequelize.sync();
--- util/database.js
+++ util/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('yourExpense','root','Vivek@#12345',{
+const sequelize = new Sequelize('yourExpense','root','Vivek@#123456',{
     dialect:'mysql',
     host:'localhost',
     logging:false
